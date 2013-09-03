59ce06832ad883e79b251db23b8ebdbc37663d6a
Mikael Auno <auno@kth.se>
2013-09-03 11:06:23 +0200

    Updated alias ll to show human readable file sizes

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -2,7 +2,7 @@ alias g=gvim\ --remote-silent
 alias diff=diff\ -u
 alias gvim=UBUNTU_MENUPROXY=\ gvim
 alias ctags=ctags-exuberant
-alias ll=ls\ -l
+alias ll=ls\ -lh
 alias la=ls\ -a
 alias l=ls
 alias lla=ls\ -la
